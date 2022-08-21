4ca0d6c4a4693cc1d925a8823d1ab705650c828a
Chester Alan <chesteralantagudin@gmail.com>
2022-08-21 17:13:06 +0800

    contact form updated

---
+++ src/api/contact.ts
@@ -0,0 +1,55 @@
+type Message = {
+    from?: string | undefined;
+    to?: string;
+    cc?: string;
+    subject?: string;
+    text?: string;
+    html?: string;
+}
+
+const sendgrid = require("@sendgrid/mail")
+//Your API Key from Sendgrid
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
+
+const message: Message = {
+  //Your authorized email from SendGrid
+  from: process.env.SENDGRID_AUTHORIZED_EMAIL,
+  to: process.env.CONTACT_FORM_EMAIL,
+  subject: process.env.CONTACT_FORM_SUBJECT,
+}
+
+const handler = (req:any, res:any) => {
+  try {
+    if (req.method !== "POST") {
+      res.json({ message: "Error" })
+    }
+
+    if (req.body) {
+      message.cc = req.body.email
+      const text:string = `${req.body.message} --- ${req.body.name} (${req.body.email})`;
+      message.text = text
+      message.html = text
+    }
+
+    return sendgrid.send(message).then(
+      () => {
+        res.status(200).json({
+          message: "Message successfully sent!",
+        })
+      },
+      (error:any) => {
+        console.error(error)
+        if (error.response) {
+          return res.status(500).json({
+            error: error.response,
+          })
+        }
+      }
+    )
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({ message: "There was an error", error: err })
+  }
+}
+
+module.exports = handler
--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,53 +1,120 @@
-import React from 'react'
+import React, { useState } from "react"
+import axios from "axios"
 
 type Props = {}
+type Message = {
+    name?: string;
+    email?: string;
+    message?: string;
+}
+
+const validateEmail = (email:string) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
 
 const Contacts = (props: Props) => {
+
+    const [active,setActive] = useState(false)
+    const [sent,setSent] = useState(false)
+    const [message,setMessage] = useState<Message>({})
+
+    const validateMessage = () => {
+        if(message === {}) {
+            setActive(false);
+            return;
+        }
+        const keys = Object.keys(message);
+        let validate = false;
+        ["name","email","message"].forEach(key => {
+            if(keys.includes(key)) {
+                validate = true;
+            } else {
+                validate = false;
+            }
+            if( (message as any)[key] === "" ) {
+                validate = false;
+            }
+            if( (message as any)[key] === undefined ) {
+                validate = false;
+            }            
+        });
+        if( (message as any)["email"] !== undefined ) {
+            if( !validateEmail((message as any)["email"]) ) {
+                validate = false;
+            }
+        }
+        setActive(validate);        
+    }
+
+    const handleChange = (event:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        if( !["name","email","message"].includes(event.target.name) ) return;
+        setMessage(Object.assign(message,{
+            [event.target.name]: event.target.value
+        }))
+        validateMessage();
+    }
+
+
+    const handleSendMessage = () => {
+        if(!active) return;
+        if(sent) return;
+        axios.post('/api/contact', message)
+          .then(function () {
+            setSent(true);
+          })
+          .catch(function (error) {
+            console.log(error);
+            setSent(true);
+          });
+    }
+
   return (
     <div className="section contacts align-left" id="contact-section">
-    <div className="fw">
+      <div className="fw">
         <div className="titles">
-            <div className="title">Contact Me</div>
+          <div className="title">Contact Me</div>
         </div>
         <div className="contact-form">
-            <form id="cform" method="post">
-                <div className="row">
-                    <div className="col col-m-12 col-t-6 col-d-6">
-                        <div className="value">
-                            <input type="text" name="name" placeholder="Name *" />
-                        </div>
-                    </div>
-                    <div className="col col-m-12 col-t-6 col-d-6">
-                        <div className="value">
-                            <input type="tel" name="tel" placeholder="Phone *" />
-                        </div>
-                    </div>
-                    <div className="col col-m-12 col-t-6 col-d-6">
-                        <div className="value">
-                            <input type="text" name="email" placeholder="Email *" />
-                        </div>
-                    </div>
-                    <div className="col col-m-12 col-t-6 col-d-6">
-                        <div className="value">
-                            <input type="text" name="subject" placeholder="Subject *" />
-                        </div>
-                    </div>
-                    <div className="col col-m-12 col-t-12 col-d-12">
-                        <div className="value">
-                            <textarea name="message" placeholder="Message"></textarea>
-                        </div>
-                    </div>
-                    <div className="col col-m-12 col-t-12 col-d-12">
-                        <a href="#" className="btn">Send Message</a>
-                    </div>
+          {!sent && <form id="cform" method="post">
+            <div className="row">
+              <div className="col col-m-12 col-t-6 col-d-6">
+                <div className="value">
+                  <input type="text" name="name" placeholder="Name *" onChange={handleChange} />
                 </div>
-            </form>
-            <div className="alert-success">
-                <p>Thanks, your message is sent successfully. We will contact you shortly!</p>
+              </div>
+              <div className="col col-m-12 col-t-6 col-d-6">
+                <div className="value">
+                  <input type="email" name="email" placeholder="Email *" onChange={handleChange} />
+                </div>
+              </div>
+              <div className="col col-m-12 col-t-12 col-d-12">
+                <div className="value">
+                  <textarea name="message" placeholder="Message" onChange={handleChange}></textarea>
+                </div>
+              </div>
+              {active && <div className="col col-m-12 col-t-12 col-d-12">
+                <a type="button" className="btn" onClick={handleSendMessage}>
+                  Send Message
+                </a>
+              </div>}
             </div>
+          </form>}
+          {sent && <div className="alert-success">
+            <p>
+              Thanks, your message is sent successfully.
+            </p>
+            <p>
+              I will contact you
+              as soon as possible!
+            </p>
+          </div>}
         </div>
+      </div>
     </div>
-</div>
   )
 }
 
